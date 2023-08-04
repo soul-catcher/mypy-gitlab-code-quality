2870e1433c3a77c6f637b3c3581d939b736fb799
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2023-08-05 00:00:29 +0700

    Partly apply linters to project

--- mypy_gitlab_code_quality/__init__.py
+++ mypy_gitlab_code_quality/__init__.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 import json
 import re
 from base64 import b64encode
-from collections.abc import Hashable, Sequence
 from sys import byteorder, hash_info, stdin
-from typing import TextIO
+from typing import TYPE_CHECKING, TextIO
+
+if TYPE_CHECKING:
+    from collections.abc import Hashable, Sequence
 
 SEVERITY = {
     "note": "info",
@@ -15,15 +17,19 @@ SEVERITY = {
 
 def get_hash(tpl: Sequence[Hashable]) -> str:
     return b64encode(
-        hash(tpl).to_bytes(hash_info.width // 8, byteorder, signed=True)
+        hash(tpl).to_bytes(hash_info.width // 8, byteorder, signed=True),
     ).decode()
 
 
 def is_info_to_previous_issue(
-    issues: list[dict], severity: str, lineNumber: int
+    issues: list[dict],
+    severity: str,
+    line_number: int,
 ) -> bool:
-    return issues and (severity == "info") and (
-        issues[-1]["location"]["lines"]["begin"] == lineNumber
+    return (
+        issues
+        and (severity == "info")
+        and (issues[-1]["location"]["lines"]["begin"] == line_number)
     )
 
 
@@ -31,11 +37,11 @@ def append_line_to_issues(
     issues: list[dict],
     fingerprint: str,
     severity: str,
-    lineNumber: int,
+    line_number: int,
     description: str,
     path: str,
 ) -> None:
-    if is_info_to_previous_issue(issues, severity, lineNumber):
+    if is_info_to_previous_issue(issues, severity, line_number):
         issues[-1]["description"] += f"\n{description}"
     else:
         issues.append(
@@ -46,10 +52,10 @@ def append_line_to_issues(
                 "location": {
                     "path": path,
                     "lines": {
-                        "begin": lineNumber,
+                        "begin": line_number,
                     },
                 },
-            }
+            },
         )
 
 
@@ -68,19 +74,14 @@ def parse_lines(lines: TextIO) -> list[dict]:
             continue
         fingerprint: str = get_hash(match.groups())
         severity: str = SEVERITY.get(match["error_level"], "unknown")
-        lineNumber: int = int(match["line"])
+        line_number: int = int(match["line"])
         description: str = match["description"]
         path: str = match["path"]
         append_line_to_issues(
-            issues, fingerprint, severity, lineNumber, description, path
+            issues, fingerprint, severity, line_number, description, path
         )
     return issues
 
 
 def main() -> None:
-    print(
-        json.dumps(
-            parse_lines(stdin),
-            indent="\t",
-        )
-    )
+    print(json.dumps(parse_lines(stdin), indent="\t"))
